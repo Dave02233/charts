1e9e7a9d3ce990c2b3ee38ccc58020bd02281736
Dave02233 <davidecannistraro02@gmail.com>
2025-08-07 17:14:05 +0200

    aiutatemi

--- charts/src/S7/OPC-UA.js
+++ charts/src/S7/OPC-UA.js
@@ -8,23 +8,12 @@ const {
 const endpointUrl = "opc.tcp://192.168.0.1:4840"; // Modifica con l’indirizzo del tuo PLC
 
 async function main() {
-    const client = OPCUAClient.create({ endpoint_must_exist: false });
+    const client = OPCUAClient.create({ endpoint_must_exist:  });
     await client.connect(endpointUrl);
 
-    const session = await client.createSession();
 
-    const nodeId = "ns=2;s=MyRealValue"; // Simbolo definito nel TIA Portal
-
-    const dataValue = await session.read({
-        nodeId,
-        attributeId: AttributeIds.Value,
-        timestampsToReturn: TimestampsToReturn.Neither
-    });
-
-    console.log("📈 Valore float simbolico:", dataValue.value.value);
 
     await session.close();
     await client.disconnect();
 }
 
-main().catch((err) => console.error("❌ Errore:", err));
